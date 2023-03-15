ea9745fd4a0c35cf2cb4f512b0b163f71ee810b2
Chidera Ike <chideraike.s@gmail.com>
2023-03-15 19:48:47 +0100

    :sparkles: created ui components `Paragraph` & `LargeHeading`

---
+++ src/app/components/ui/LargeHeading.tsx
@@ -0,0 +1,39 @@
+import { forwardRef, HTMLAttributes } from 'react'
+import { cva, VariantProps } from 'class-variance-authority'
+import { cn } from '@/app/lib/utils'
+
+const headingVariants = cva(
+  'text-black dark:text-white text-center lg:text-left font-extrabold leading-tight tracking-tighter',
+  {
+    variants: {
+      size: {
+        default: 'text-4xl md:text-5xl lg:text-6xl',
+        sm: 'text-2xl md:text-3xl lg:text-4xl',
+        lg: 'text-5xl md:text-6xl lg:text-7xl',
+      },
+    },
+    defaultVariants: {
+      size: 'default',
+    },
+  }
+)
+
+interface LargeHeadingProps
+  extends HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof headingVariants> {}
+
+const LargeHeading = forwardRef<HTMLHeadingElement, LargeHeadingProps>(
+  function LargeHeading({ className, size, children, ...props }, ref) {
+    return (
+      <h1
+        {...props}
+        ref={ref}
+        className={cn(headingVariants({ size, className }))}
+      >
+        {children}
+      </h1>
+    )
+  }
+)
+
+export default LargeHeading
